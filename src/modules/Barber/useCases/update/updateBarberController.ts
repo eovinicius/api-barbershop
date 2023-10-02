96676184c46add10fb4d07bfdfae8daad32b7aad
vini <voliveirasantosss@gmail.com>
2023-10-02 11:22:04 -0300

    commit

--- src/modules/Barber/useCases/update/updateBarberController.ts
+++ src/modules/Barber/useCases/update/updateBarberController.ts
@@ -1,3 +1,22 @@
-export class UpdateBarberController {
+import { Request, Response } from 'express';
+import { UpdateBarberUseCase } from './updateBarberUseCase';
+import { BarberRepository } from '../../repositories/prisma/barberRepository';
 
+interface IRequest {
+  name: string;
+  photo: string;
+}
+
+export class UpdateBarberController {
+  static async handle(request: Request, response: Response): Promise<Response> {
+    const { name, photo }: IRequest = request.body;
+    const { id } = request.params;
+
+    const barberRepository = new BarberRepository();
+    const updateBarberUseCase = new UpdateBarberUseCase(barberRepository);
+
+    await updateBarberUseCase.execute({ id, name, photo });
+
+    return response.status(204).send();
+  }
 }
